7bf47995c5101559adf87beb5bfb37fe8c62c218
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-03-18 02:43:07 -0600

    Added functionality to add a new note, but new iss
    Whenever the data loads on mount, it comes back
    null.

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useFetch from '../utils/useFetch';
+import { v4 as uuidv4 } from 'uuid';
 import '../styles/Notes.css';
 
 export default function Notes() {
-	const data = useFetch();
+	// TODO: Fetched data is null on mount.
+	const fetchedData = useFetch();
+	console.log(fetchedData);
+	const [data, setData] = useState(fetchedData);
+	console.log(data);
 	const [showNewNote, setShowNewNote] = useState(false);
 	const [title, setTitle] = useState('');
 	const [body, setBody] = useState('');
@@ -13,12 +18,69 @@ export default function Notes() {
 		setShowNewNote(!showNewNote);
 	}
 
+	function handleSubmit(e) {
+		e.preventDefault();
+		// Grab inputs
+		const titleInput = document.querySelector('#title');
+		const bodyInput = document.querySelector('#body');
+		let titleInputError = document.querySelector(
+			'.add-new-note-form form div:nth-child(1) p'
+		);
+		let bodyInputError = document.querySelector(
+			'.add-new-note-form form div:nth-child(2) p'
+		);
+
+		// Validate inputs
+		if (titleInput.value.trim() === '') {
+			titleInputError.textContent = 'Please enter a title for the note.';
+			titleInput.textContent = '';
+			setTitle('');
+		} else {
+			titleInputError.textContent = '';
+		}
+
+		if (bodyInput.value.trim() === '') {
+			bodyInputError.textContent = 'Please enter a body for the note.';
+			bodyInput.value = '';
+			setBody('');
+		} else {
+			bodyInputError.textContent = '';
+		}
+
+		if (bodyInput.value.trim() === '' || titleInput.value.trim() === '') {
+			return;
+		}
+
+		// Create new note
+		try {
+			fetch('/api/notes', {
+				method: 'POST',
+				body: JSON.stringify({
+					id: uuidv4().toString(),
+					title: titleInput.value,
+					body: bodyInput.value,
+				}),
+			}).then(() => {
+				// refresh component with new notes
+				fetch('/api/notes')
+					.then((response) => response.json())
+					.then((json) => setData(json.notes));
+			});
+		} catch (error) {
+			console.error('Error creating new note: ' + error);
+		}
+		handleShowNewNote();
+		setTitle('');
+		setBody('');
+	}
+
 	function addNewNoteForm() {
 		return (
 			<section className='add-new-note-form'>
 				<form>
 					<div>
 						<label htmlFor='title'>Title:</label>
+						<p className='error'></p>
 						<input
 							type='text'
 							name='title'
@@ -29,17 +91,20 @@ export default function Notes() {
 					</div>
 					<div>
 						<label htmlFor='body'>Body:</label>
+						<p className='error'></p>
 						<textarea
 							name='body'
 							id='body'
 							cols='30'
 							rows='10'
-							onChange={(e) => setBody(e.target.boy)}
+							onChange={(e) => setBody(e.target.value)}
 							value={body}
 						></textarea>
 					</div>
 					<div>
-						<button type='submit'>Create new note</button>
+						<button type='submit' onClick={(e) => handleSubmit(e)}>
+							Create new note
+						</button>
 					</div>
 				</form>
 			</section>
